fe420710b387bfeba83131211d13360ff755b3ef
RohanGd <rohangadgil28@gmail.com>
2026-01-11 13:20:55 +0100

    parameterized as dict the dataloader mask_dirs, imgs_dir

--- emrDataloader.py
+++ emrDataloader.py
@@ -6,19 +6,19 @@ class DataloaderBuilder:
     def __init__(self, cfg, logger):
         self.cfg = cfg
         self.logger = logger
-        self.imgs_dir, self.masks_dir = None, None
+        self.imgs_dir, self.masks_dir = dict(), dict()
 
     def build(self, mode="train"):
         dataset = self.cfg.get("DATASET", "dataset_name")
-        self.imgs_dir = "datasets/" + dataset + "/" + mode + "/imgs"
-        self.masks_dir = "datasets/" + dataset + "/" + mode + "/masks"
+        self.imgs_dir[mode] = "datasets/" + dataset + "/" + mode + "/imgs"
+        self.masks_dir[mode] = "datasets/" + dataset + "/" + mode + "/masks"
 
         num_slices_per_batch = self.cfg.get_int("MODEL", "num_slices_per_batch")
         batch_size = self.cfg.get_int("LOOP", "batch_size", 1) 
 
         dataset = emrDataset(
-            imgs_dir=self.imgs_dir,
-            masks_dir=self.masks_dir,
+            imgs_dir=self.imgs_dir[mode],
+            masks_dir=self.masks_dir[mode],
             n=num_slices_per_batch,
             logger=self.logger,
             mode=mode
@@ -37,7 +37,7 @@ class DataloaderBuilder:
         )
 
         self.logger.info(
-            f"Train loader: {len(dataloader)} batches | "
+            f"Initialized {mode} loader: {len(dataloader)} batches | "
             f"Total slices: {len(dataset)} | batch_size={batch_size}"
         )
 
