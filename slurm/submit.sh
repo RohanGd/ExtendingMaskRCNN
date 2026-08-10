4584e31e813e15e5de447ddbc1a63e1fcec9960f
RohanGd <rohangadgil28@gmail.com>
2026-08-10 22:51:56 +0200

    nochange

--- slurm/submit.sh
+++ slurm/submit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=emaskrcnn
+#SBATCH --job-name=base_n1_MouseOrganoid
 #SBATCH --partition=RTX3090
 #SBATCH --gpus=1
 #SBATCH --mem=30G
 #SBATCH --ntasks=1
 #SBATCH --time=11:00:00
-#SBATCH --cpus-per-task=4
-#SBATCH --output=/netscratch/gadgil/runlogs/Emaskrcnn_%j.log
+#SBATCH --cpus-per-task=8
+#SBATCH --output=/netscratch/gadgil/runlogs/%x_%j.log
 
 srun \
   --container-image=/netscratch/gadgil/extending_maskrcnn_v1.sqsh \
   --container-mounts=/home/gadgil/ExtendingMaskRCNN:/home/gadgil/ExtendingMaskRCNN,/netscratch/gadgil:/netscratch/gadgil,/ds:/ds:ro \
   --container-workdir=/home/gadgil/ExtendingMaskRCNN \
-  python3 training_loop.py config/channelFusion.ini
+  python3 training_loop.py config/base_n1_MouseOrganoid.ini
